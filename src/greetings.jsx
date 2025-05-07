d696db5f9524a6138ddf09d98a77de5889461bf5
Rohit <rohitjames77@outlook.com>
2025-05-07 19:21:07 +0530

    Complete Jsx exercise

---
+++ src/greetings.jsx
@@ -0,0 +1,13 @@
+function Greetings() {
+  return <h1>&quot;Hello to the React World. &quot;</h1>;
+}
+
+function Svg() {
+  return (
+    <svg>
+      <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+    </svg>
+  );
+}
+
+export {Greetings, Svg};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Greetings, Svg } from "./greetings.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div>
+      <Greetings />
+      <Svg />
+    </div>
+  </StrictMode>
+);
